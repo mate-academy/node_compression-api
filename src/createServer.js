74918855eeb618d15f55bd3bfb4674004c9f9a4b
Yevhenii <polotnyak2004@gmail.com>
2024-07-08 16:08:28 +0300

    fix

--- src/createServer.js
+++ src/createServer.js
@@ -11,26 +11,36 @@ function createServer() {
   const server = new http.Server();
 
   server.on('request', (req, res) => {
-    const url = new URL(req.url, `http://${req.headers.host}`);
+    const { pathname } = new URL(req.url, `http://${req.headers.host}`);
     const form = new formidable.IncomingForm();
 
-    if (url.pathname !== '/compress') {
+    if (pathname === '/' && req.method === 'GET') {
+      fs.readFile('./public/index.html', (err, data) => {
+        if (err) {
+          res.statusCode = 404;
+          res.end('Not found');
+
+          return;
+        }
+
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'text/html');
+        res.end(data);
+      });
+
+      return;
+    }
+
+    if (pathname !== '/compress') {
       res.statusCode = 404;
       res.end('Trying to access a non-existing route!');
 
       return;
     }
 
-    if (url.req.method !== 'POST') {
+    if (req.method !== 'POST') {
       res.statusCode = 400;
       res.end('Use POST request method instead!');
-
-      return;
-    }
-
-    if (url.pathname === '/' && req.method === 'GET') {
-      res.statusCode = 200;
-      res.end('Ready!');
 
       return;
     }
@@ -47,7 +57,7 @@ function createServer() {
       const [file] = files;
 
       if (!compressionTypes.includes(compressionType)) {
-        res.statusCode = 404;
+        res.statusCode = 400;
         res.end('Bad Request: Compression type not supported');
 
         return;
