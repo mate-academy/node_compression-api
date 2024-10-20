1eed26bd548dc0933c6eff003c4f5f3af2bb03f3
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2024-10-20 22:13:38 +0300

    7 test passed

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,96 @@
+/* eslint-disable no-console */
 'use strict';
 
+const { Server } = require('http');
+const { pipeline } = require('stream');
+const fs = require('fs');
+// const mime = require('mime-types');
+// const path = require('path');
+const zlib = require('zlib');
+const formidable = require('formidable');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = new Server();
+
+  server.on('request', (req, res) => {
+    if (req.url === '/') {
+      res.setHeader('Content-Type', 'text/html');
+      fs.createReadStream('./public/index.html').pipe(res);
+
+      return;
+    }
+
+    if (req.url === '/compress' && req.method === 'POST') {
+      const form = new formidable.IncomingForm();
+
+      const compressionTypes = {
+        gzip: zlib.createGzip,
+        br: zlib.createBrotliCompress,
+        deflate: zlib.createDeflate,
+      };
+
+      form.parse(req, (err, fields, files) => {
+        const compressionType = fields.compressionType;
+        const file = files.file;
+
+        if (
+          !compressionType ||
+          !file ||
+          err ||
+          !compressionTypes.hasOwnProperty(compressionType)
+        ) {
+          res.statusCode = 400;
+          res.end('Invalid form data');
+
+          return;
+        }
+
+        res.setHeader('Content-encoding', compressionType);
+
+        res.setHeader(
+          'Content-Disposition',
+          `attachment; filename=${file[0].originalFilename}.${compressionType[0]}`,
+        );
+
+        console.log(compressionType);
+        console.log(file);
+
+        const fileStream = fs.createReadStream(file[0].filepath);
+        const compressionStream = compressionTypes[compressionType[0]]();
+
+        // fileStream
+        //   .on('error', () => {})
+        //   .pipe(compressionStream)
+        //   .on('error', () => {})
+        //   .pipe(res)
+        //   .on('error', () => {});
+
+        pipeline(fileStream, compressionStream, res, (error) => {
+          if (error) {
+            // eslint-disable-next-line no-console
+            console.error('Pipeline failed', error);
+          }
+        });
+
+        res.on('close', () => fileStream.destroy());
+
+        res.statusCode = 200;
+        res.end('Form received and logged');
+      });
+    } else {
+      res.statusCode = 404;
+
+      res.end();
+    }
+  });
+
+  server.on('error', (error) => {
+    console.error('Server error:', error.message);
+  });
+
+  server.on('close', () => console.log('Server is closed'));
+
+  return server;
 }
 
 module.exports = {
