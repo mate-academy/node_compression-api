40fd42fa81748b575e4af687318034b6dbb8b55e
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2024-10-20 23:27:15 +0300

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,21 +1,28 @@
-/* eslint-disable no-console */
 'use strict';
 
-const { Server } = require('http');
-const { pipeline } = require('stream');
+const http = require('http');
 const fs = require('fs');
-// const mime = require('mime-types');
-// const path = require('path');
 const zlib = require('zlib');
 const formidable = require('formidable');
+const { pipeline } = require('stream');
 
 function createServer() {
-  const server = new Server();
+  const server = new http.Server();
 
   server.on('request', (req, res) => {
-    if (req.url === '/') {
+    if (req.url === '/' && req.method === 'GET') {
+      res.statusCode = 200;
       res.setHeader('Content-Type', 'text/html');
       fs.createReadStream('./public/index.html').pipe(res);
+
+      return;
+    }
+
+    if (req.url === '/compress' && req.method !== 'POST') {
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'text/plain');
+
+      res.end('Requests to /compress need to be POST method');
 
       return;
     }
@@ -23,72 +30,64 @@ function createServer() {
     if (req.url === '/compress' && req.method === 'POST') {
       const form = new formidable.IncomingForm();
 
-      const compressionTypes = {
-        gzip: zlib.createGzip,
-        br: zlib.createBrotliCompress,
-        deflate: zlib.createDeflate,
-      };
+      form.parse(
+        req,
+        (err, { compressionType: compressionTypes }, { file: files }) => {
+          if (!files || !compressionTypes || err) {
+            res.statusCode = 400;
+            res.end('Form error');
 
-      form.parse(req, (err, fields, files) => {
-        const compressionType = fields.compressionType;
-        const file = files.file;
-
-        if (
-          !compressionType ||
-          !file ||
-          err ||
-          !compressionTypes.hasOwnProperty(compressionType)
-        ) {
-          res.statusCode = 400;
-          res.end('Invalid form data');
-
-          return;
-        }
-
-        res.setHeader('Content-encoding', compressionType);
-
-        res.setHeader(
-          'Content-Disposition',
-          `attachment; filename=${file[0].originalFilename}.${compressionType[0]}`,
-        );
-
-        console.log(compressionType);
-        console.log(file);
-
-        const fileStream = fs.createReadStream(file[0].filepath);
-        const compressionStream = compressionTypes[compressionType[0]]();
-
-        // fileStream
-        //   .on('error', () => {})
-        //   .pipe(compressionStream)
-        //   .on('error', () => {})
-        //   .pipe(res)
-        //   .on('error', () => {});
-
-        pipeline(fileStream, compressionStream, res, (error) => {
-          if (error) {
-            // eslint-disable-next-line no-console
-            console.error('Pipeline failed', error);
+            return;
           }
-        });
 
-        res.on('close', () => fileStream.destroy());
+          const compressionType = compressionTypes[0];
+          const file = files[0];
 
-        res.statusCode = 200;
-        res.end('Form received and logged');
-      });
-    } else {
-      res.statusCode = 404;
+          const compressors = {
+            gzip: zlib.createGzip,
+            br: zlib.createBrotliCompress,
+            deflate: zlib.createDeflate,
+          };
 
-      res.end();
+          if (!compressors.hasOwnProperty(compressionType)) {
+            res.statusCode = 400;
+            res.end('Unsupported compression type');
+
+            return;
+          }
+
+          const compressorType = compressors[compressionType]();
+          const fileStream = fs.createReadStream(file.filepath);
+
+          res.statusCode = 200;
+
+          res.setHeader(
+            'Content-Disposition',
+            `attachment; filename=${file.originalFilename}.${compressionType}`,
+          );
+
+          pipeline(fileStream, compressorType, res, (error) => {
+            if (error) {
+              // eslint-disable-next-line no-console
+              console.error('Pipeline failed', error);
+            }
+          });
+
+          res.on('close', () => {
+            compressorType.destroy();
+          });
+        },
+      );
+
+      return;
     }
+
+    res.statusCode = 404;
+    res.end('Page not found');
   });
 
-  server.on('error', (error) => {
-    console.error('Server error:', error.message);
-  });
-
-  server.on('close', () => console.log('Server is closed'));
+  // eslint-disable-next-line no-console
+  server.on('error', (error) => console.log(error));
 
   return server;
 }
