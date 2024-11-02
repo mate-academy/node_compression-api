f194c711fb9063ede574cedcc52aaa04f3f3680e
mrsvolodya <vovamursky@gmail.com>
2024-11-02 22:11:24 +0200

    node-compression-app

--- src/createServer.js
+++ src/createServer.js
@@ -4,13 +4,18 @@
 
 const http = require('node:http');
 const fs = require('node:fs');
-const server = new http.Server();
 const path = require('node:path');
-const { formidable, errors: formidableErrors } = require('formidable');
-const zlib = require('node:zlib');
+const { formidable } = require('formidable');
+const {
+  createGzip,
+  createBrotliCompress,
+  createDeflate,
+} = require('node:zlib');
 
 function createServer() {
-  return server.on('request', async (req, res) => {
+  const server = new http.Server();
+
+  server.on('request', async (req, res) => {
     if (req.url === '/favicon.ico') {
       res.statusCode = 200;
       res.end('');
@@ -41,74 +46,88 @@ function createServer() {
       });
 
       res.on('close', () => fileStream.destroy());
+
+      return;
     }
 
     if (req.method === 'POST' && req.url === '/compress') {
-      const gzip = zlib.createGzip();
-      const br = zlib.createBrotliCompress();
-      const deflate = zlib.createDeflate();
-
       const form = formidable({});
-      let fields;
-      let files;
+      let fields, files;
 
       try {
         [fields, files] = await form.parse(req);
       } catch (err) {
-        if (err.code === formidableErrors.maxFieldsExceeded) {
-        }
         res.writeHead(err.httpCode || 400, { 'Content-Type': 'text/plain' });
         res.end(String(err));
 
         return;
       }
 
-      const compressionType = fields.compressionType[0];
-      let compressionData;
-      let extension;
+      const compressionType = Array.isArray(fields.compressionType)
+        ? fields.compressionType[0]
+        : fields.compressionType;
+
+      const filePath = Array.isArray(files.file)
+        ? files.file[0].filepath
+        : files.file;
+      const fileName = Array.isArray(files.file)
+        ? files.file[0].originalFilename
+        : files.file;
+
+      if (!filePath || !fileName || !compressionType) {
+        res.statusCode = 400;
+        res.end();
+
+        return;
+      }
+
+      if (!fs.existsSync(filePath)) {
+        res.statusCode = 404;
+        res.end('File not found!');
+      }
+
+      let compressionData, extension;
 
       switch (compressionType) {
         case 'gzip':
-          compressionData = gzip;
-          extension = '.gz';
+          compressionData = createGzip();
+          res.setHeader('Content-Encoding', 'gz');
+          extension = 'gzip';
           break;
         case 'deflate':
-          compressionData = deflate;
-          extension = '.dfl';
+          compressionData = createDeflate();
+          res.setHeader('Content-Encoding', '.dfl');
+          extension = 'deflate';
           break;
         case 'br':
-          compressionData = br;
-          extension = '.br';
+          compressionData = createBrotliCompress();
+          res.setHeader('Content-Encoding', '.br');
+          extension = 'br';
           break;
         default:
-          res.writeHead(400, { 'Content-Type': 'text/plain' });
+          res.statusCode = 400;
           res.end();
 
           return;
       }
 
-      res.writeHead(
-        200,
-        'Content-Disposition',
-        `attachment; filename=file${extension}`,
-      );
-
-      const filePath = files.file[0].filepath;
-
-      if (!fs.existsSync(filePath)) {
-        console.log('FILE NOT FOUND');
-        res.statusCode = 404;
-        res.end('File not found!');
-      }
-
-      console.log(filePath);
-
       const fileStream = fs.createReadStream(filePath);
 
-      fileStream.pipe(compressionData.pipe(res));
+      res.statusCode = 200;
 
-      // res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end();
+      res.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Content-Disposition': `attachment; filename=${fileName}.${extension}`,
+      });
+      console.log(fields, files, req.method, compressionType);
+
+      fileStream
+        .pipe(compressionData)
+        .pipe(res)
+        .on('error', (err) => {
+          res.statusCode = 500;
+          res.end(`Stream error: ${err.message}`);
+        });
 
       return;
     }
@@ -118,6 +137,8 @@ function createServer() {
       res.end();
     }
   });
+
+  return server;
 }
 
 module.exports = {
